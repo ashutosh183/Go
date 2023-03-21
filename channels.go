e90a4dea39a6c825a704893b5b61ca661dad9d12
Ashutosh Pandey <112218446+ashutosh-183@users.noreply.github.com>
2023-03-21 22:03:20 +0530

    channels

---
+++ channels.go
@@ -0,0 +1,28 @@
+package main
+
+import(
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main(){
+	ch := make(chan int)
+
+	wg.Add(2)
+
+	go func(){
+		i := <- ch
+		fmt.Println(i)
+		wg.Done()
+	}()
+
+	go func(){
+		ch <- 100
+		wg.Done()
+
+	}()
+
+	wg.Wait()
+}
